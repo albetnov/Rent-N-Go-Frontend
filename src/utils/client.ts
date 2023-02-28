e0eab6114b7200ae78a42557a439b58780cf91b6
albetnov <ryxinfrvr@gmail.com>
2023-02-28 23:00:44 +0700

    chire: adjustment on refresh token

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -18,66 +18,78 @@ const getTokens = () => {
   return null;
 };
 
-client.interceptors.request.use((api) => {
+const refreshToken = async () => {
   const token = getTokens();
+  try {
+    const result = await client.post("/auth/refresh", { refreshToken: token.refresh_token });
+    localStorage.setItem("tokens", JSON.stringify(result.data));
+  } catch (err) {
+    localStorage.removeItem("tokens");
+  }
+};
+
+client.interceptors.request.use(async (api) => {
+  let token = getTokens();
 
   if (!token) {
     localStorage.removeItem("tokens");
     return api;
   }
 
-  api.headers.Authorization = `Bearer ${token.accessToken}`;
+  if (token.token_expired_at < Date.now() && token.refresh_token_expired_at > Date.now()) {
+    await refreshToken();
+    token = getTokens();
+  }
+
+  api.headers.Authorization = `Bearer ${token.token}`;
 
   return api;
 });
 
-client.interceptors.response.use(
-  (config) => config,
-  async (error) => {
-    if (error.response.status >= 500) {
-      toast({
-        title: "Error",
-        description: "Something went wrong. Please try again later.",
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-        position: "top-right",
-      });
-    }
-
-    if (error.response.status === 400 && error.response.data.action === "INVALID_PAYLOAD") {
-      const errors = error.response.data.errors.map(
-        (item: any) => `${item.FailedFields.split(".")[1]} is ${item.Tag}`
-      );
-
-      toast({
-        title: "Error sending data",
-        description: "Please check the fields and try again: " + errors.join(", "),
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "top-right",
-      });
-    }
-
-    if (error.response.status === 401) {
-      const token = getTokens();
-
-      if (!token) {
-        localStorage.removeItem("tokens");
-        return Promise.reject(error);
-      }
-
-      try {
-        const result = await client.post("/auth/refresh", { refreshToken: token.refreshToken });
-        localStorage.setItem("tokens", JSON.stringify(result.data));
-      } catch (err) {
-        localStorage.removeItem("tokens");
-      }
-    }
-
-    return Promise.reject(error);
+client.interceptors.response.use(undefined, async (error) => {
+  if (error.response.status >= 500) {
+    toast({
+      title: "Error",
+      description: "Something went wrong. Please try again later.",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top-right",
+    });
   }
-);
+
+  if (error.response.status === 400 && error.response.data.action === "INVALID_PAYLOAD") {
+    const errors = error.response.data.errors.map(
+      (item: any) => `${item.FailedFields.split(".")[1]} is ${item.Tag}`
+    );
+
+    toast({
+      title: "Error sending data",
+      description: "Please check the fields and try again: " + errors.join(", "),
+      status: "error",
+      duration: 5000,
+      isClosable: true,
+      position: "top-right",
+    });
+  }
+
+  if (error.response.status === 401) {
+    const token = getTokens();
+
+    if (!token) {
+      localStorage.removeItem("tokens");
+      return Promise.reject(error);
+    }
+
+    try {
+      await refreshToken();
+      return axios(error.config);
+    } catch (err) {
+      localStorage.removeItem("tokens");
+    }
+  }
+
+  return Promise.reject(error);
+});
 
 export default client;
