76301980254be0a6baaf8776df70364277f8952b
DJason28 <delvinjason28.tio@gmail.com>
2023-03-03 22:09:43 +0700

    Merge with main

--- src/pages/Services/Drivers.tsx
+++ src/pages/Services/Drivers.tsx
@@ -33,3 +33,59 @@ export default function Drivers() {
     </Layout>
   )
 }
+
+export default function Drivers(){
+    useCustomBackground(colors.secondary)
+    return(
+        <Layout>
+            <Flex mt={50} px={30} gap={5} >
+            <SearchBars />
+            <DriverMenu />
+            </Flex>
+            <Box px={30}>
+            <Box mt='30px' boxShadow='dark-lg'>
+                    <DriverCard 
+                    name='Atnan Ari Anderson' 
+                    stars={4}
+                    imgUrl='https://bit.ly/dan-abramov'
+                    features={DRIVER_FEATURE}                    />
+            </Box>
+            <Box mt='30px' boxShadow='dark-lg'>
+                    <DriverCard 
+                    name='Albet Novendo'
+                    stars={5}
+                    imgUrl='https://bit.ly/dan-abramov' 
+                    features={DRIVER_FEATURE}                    />
+            </Box>
+            <Box mt='30px' boxShadow='dark-lg'>
+                    <DriverCard 
+                    name='Delvin Jason'
+                    stars={4}
+                    imgUrl='https://bit.ly/dan-abramov' 
+                    features={DRIVER_FEATURE}                      />
+            </Box>
+            <Box mt='30px' boxShadow='dark-lg'>
+            <DriverCard 
+                    name='Kelvin'
+                    stars={5}
+                    imgUrl='https://bit.ly/dan-abramov' 
+                    features={DRIVER_FEATURE}                      />
+            </Box>
+            <Box mt='30px' boxShadow='dark-lg'>
+                    <DriverCard 
+                    name='Mieko Huang Vincent'
+                    stars={3}
+                    imgUrl='https://bit.ly/dan-abramov' 
+                    features={DRIVER_FEATURE}                      />
+            </Box>
+            <Box mt='30px' boxShadow='dark-lg'>
+                <DriverCard 
+                    name='Rayson'
+                    stars={4}
+                    imgUrl='https://bit.ly/dan-abramov' 
+                    features={DRIVER_FEATURE}                      />
+            </Box>
+            </Box>
+        </Layout>
+    )
+}
