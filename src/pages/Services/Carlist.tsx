0c176ef1e4dacdd668008ad8976e3f70dcfbc9f7
albetnov <ryxinfrvr@gmail.com>
2023-03-05 13:26:48 +0700

    refactor: split card into own component, slight adjustment on padding and gap

--- src/pages/Services/Carlist.tsx
+++ src/pages/Services/Carlist.tsx
@@ -1,105 +1,34 @@
 import Layout from '../../components/Layout'
 import CarCard from '../../components/Cars/CarCard'
 import { Box, Flex } from '@chakra-ui/react'
-import { Select } from '@chakra-ui/react'
 import SearchBar from '../../components/Searchbar'
 import BrandFilter from '../../components/Cars/BrandFilter'
 import PriceFilter from '../../components/Cars/PriceFilter'
 import SeatFilter from '../../components/Cars/SeatFilter'
+import CarCardShadow from '../../components/Cars/CarCardShadow'
 
 export default function CarList() {
   return (
     <Layout>
-      <Flex mt={50} gap={6} px={70}>
+      <Flex mt={50} gap={6} justifyContent="center">
         <SearchBar />
 
         <BrandFilter />
         <SeatFilter />
         <PriceFilter />
       </Flex>
-      <Box px={70}>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Lexus LC 500"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Mazda Rx 7"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Mustang"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Lexus LC 500"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Avanza Toyota"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Suzuki Apv"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Subaru"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Ferrari"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-        <Box mr="70px" mt="50px" boxShadow="dark-lg">
-          <CarCard
-            name="Jeep"
-            imgUrl="https://bit.ly/dan-abramov"
-            seats={4}
-            baggages={2}
-            stock={10}
-          />
-        </Box>
-      </Box>
+      <Flex my={10} gap={14} flexDir="column">
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+        <CarCardShadow />
+      </Flex>
     </Layout>
   )
 }
