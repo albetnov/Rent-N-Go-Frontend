e1bf2061c7d7b6736dc3d0654ffeb41238716d44
albetnov <ryxinfrvr@gmail.com>
2023-03-06 10:24:51 +0700

    feat: add faq buttona and logo button handler

--- src/components/Topbar/Logo.tsx
+++ src/components/Topbar/Logo.tsx
@@ -1,7 +1,21 @@
 import { Image } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Logo() {
+  const navigate = useNavigate()
+
+  const onLogoClicked = () => {
+    navigate('/')
+  }
+
   return (
-    <Image src="/logo.webp" alt="Rent-N-Go Logo" width="156px" height="40px" />
+    <Image
+      onClick={onLogoClicked}
+      src="/logo.webp"
+      alt="Rent-N-Go Logo"
+      width="156px"
+      height="40px"
+      _hover={{ cursor: 'pointer' }}
+    />
   )
 }
