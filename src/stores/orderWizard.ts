f0b182f8c7e68409dccd0527a75344a82e027634
albetnov <ryxinfrvr@gmail.com>
2023-03-14 13:10:25 +0700

    refactor: make order of each wizard can be multiple item

--- src/stores/orderWizard.ts
+++ src/stores/orderWizard.ts
@@ -2,9 +2,10 @@ import { create } from 'zustand'
 
 interface OrderItem {
   module: 'tours' | 'cars' | 'drivers'
-  tourId?: number
-  carId?: number
-  driverId?: number
+  photo: string
+  name: string
+  price: number
+  licensePlate?: string
 }
 
 interface OrderWizardStore {
@@ -21,31 +22,35 @@ interface OrderWizardStore {
     returnLocation: string
   ) => void
   getLocation: string | null
-  item: OrderItem | null
+  item: OrderItem[] | null
   finishSecondFlow: () => void
   finishThirdFlow: () => void
-  orderTour: (tourId: number) => void
-  orderDriver: (driverId: number) => void
-  orderCar: (carId: number) => void
+  orderTour: (tourId: number) => Promise<void>
+  orderDriver: (driverId: number, carId: number) => Promise<void>
+  orderCar: (carId: number) => Promise<void>
 }
 
+const WIZARD_STEP = 'wizard_step'
+const ORDER_LOCATION = 'order_location'
+const ORDER_ITEM = 'order_item'
+
 const useOrderWizardStore = create<OrderWizardStore>((set) => ({
-  step: parseInt(localStorage.getItem('wizard_step') ?? '0'),
-  hasOrder: !!localStorage.getItem('wizard_step'),
-  getLocation: localStorage.getItem('order_location'),
-  item: localStorage.getItem('order_item')
-    ? (JSON.parse(localStorage.getItem('order_item') as string) as OrderItem)
+  step: parseInt(localStorage.getItem(WIZARD_STEP) ?? '0'),
+  hasOrder: !!localStorage.getItem(WIZARD_STEP),
+  getLocation: localStorage.getItem(ORDER_LOCATION),
+  item: localStorage.getItem(ORDER_ITEM)
+    ? (JSON.parse(localStorage.getItem(ORDER_ITEM) as string) as OrderItem[])
     : null,
 
   enableOrder() {
-    localStorage.setItem('wizard_step', '1')
+    localStorage.setItem(WIZARD_STEP, '1')
     set(() => ({ hasOrder: true, step: 1 }))
   },
 
   doneOrder() {
-    localStorage.removeItem('order_location')
-    localStorage.removeItem('wizard_step')
-    localStorage.removeItem('order_item')
+    localStorage.removeItem(ORDER_LOCATION)
+    localStorage.removeItem(WIZARD_STEP)
+    localStorage.removeItem(ORDER_ITEM)
     set(() => ({ hasOrder: false, step: 0, item: null, getLocation: null }))
   },
 
@@ -65,50 +70,92 @@ const useOrderWizardStore = create<OrderWizardStore>((set) => ({
       returnLocation
     })
 
-    localStorage.setItem('order_location', payload)
-    localStorage.setItem('wizard_step', '2')
+    localStorage.setItem(ORDER_LOCATION, payload)
+    localStorage.setItem(WIZARD_STEP, '2')
 
     set(() => ({ step: 2, getLocation: payload }))
   },
 
   finishSecondFlow() {
-    localStorage.setItem('wizard_step', '3')
+    localStorage.setItem(WIZARD_STEP, '3')
     set(() => ({ step: 3 }))
   },
 
   finishThirdFlow() {
-    localStorage.setItem('wizard_step', '4')
+    localStorage.setItem(WIZARD_STEP, '4')
     set(() => ({ step: 4 }))
   },
 
-  orderTour(tourId) {
-    const item: OrderItem = {
-      module: 'tours',
-      tourId
-    }
+  async orderTour(tourId) {
+    // TODO: fetch the tour data
+    // const tourData = await client.get(tourId)
 
-    localStorage.setItem('order_item', JSON.stringify(item))
+    // TODO: map the tour data
+    const item: OrderItem[] = [
+      {
+        module: 'tours',
+        photo: 'photo',
+        name: '',
+        price: 1
+      }
+    ]
+
+    // TODO: fetch the driver data and append it to order item
+    // const driverData = await client.get(tourData.driverId)
+    // item.push({})
+
+    // TODO: fetch the car data and append it to order item
+    // const carData  = await client.get(tourData.carId)
+    // item.push({})
+
+    localStorage.setItem(WIZARD_STEP, '2')
+    // TODO: set the location of the order to the tour location
+    // localStorage.setItem(ORDER_LOCATION, JSON.stringify({}))
+    localStorage.setItem(ORDER_ITEM, JSON.stringify(item))
     set(() => ({ item }))
   },
 
-  orderCar(carId) {
-    const item: OrderItem = {
-      module: 'cars',
-      carId
-    }
+  async orderCar(carId) {
+    // TODO: fetch the car data
+    // const result = await client.get(carId)
 
-    localStorage.setItem('order_item', JSON.stringify(item))
-    set(() => ({ item }))
+    // TODO: map the car data
+    const item: OrderItem[] = [
+      {
+        module: 'cars',
+        name: '',
+        photo: '',
+        price: 0,
+        licensePlate: ''
+      }
+    ]
+
+    localStorage.setItem(WIZARD_STEP, '1')
+    localStorage.setItem(ORDER_ITEM, JSON.stringify(item))
+    set(() => ({ item, step: 1 }))
   },
 
-  orderDriver(driverId) {
-    const item: OrderItem = {
-      module: 'drivers',
-      driverId
-    }
+  async orderDriver(driverId, carId) {
+    // TODO: fetch the driver data
+    // const result = await client.get(driverId)
 
-    localStorage.setItem('order_item', JSON.stringify(item))
-    set(() => ({ item }))
+    // TODO: map the driver data
+    const item: OrderItem[] = [
+      {
+        module: 'drivers',
+        name: '',
+        photo: '',
+        price: 0
+      }
+    ]
+
+    // TODO: fetch the car data and append it to order item
+    // const carData  = await client.get(carId)
+    // item.push({})
+
+    localStorage.setItem(WIZARD_STEP, '1')
+    localStorage.setItem(ORDER_ITEM, JSON.stringify(item))
+    set(() => ({ item, step: 1 }))
   }
 }))
 
