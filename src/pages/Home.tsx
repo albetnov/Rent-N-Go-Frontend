ff4057a1e620c04c856a02643e618a23109e5da1
delvin jason <delvinjason28.tio@gmail.com>
2023-06-10 16:24:28 +0700

    banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,13 +28,13 @@ export default function Home() {
           slidesPerView={1}
         >
           <SwiperSlide>
-            <ImageSlide url="https://source.unsplash.com/1920x1080?travel" />
+            <ImageSlide url="baneeer1.jpg" />
           </SwiperSlide>
           <SwiperSlide>
-            <ImageSlide url="https://source.unsplash.com/1920x1080?travel" />
+            <ImageSlide url="baneeer2.jpg" />
           </SwiperSlide>
           <SwiperSlide>
-            <ImageSlide url="https://source.unsplash.com/1920x1080?travel" />
+            <ImageSlide url="baneeer3.jpg" />
           </SwiperSlide>
         </Swiper>
       </Box>
